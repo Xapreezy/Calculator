b371fb58df5c2c6bcf611876c2769bff98e32a8e
Xapreezy <xapreezy@Gmail.com>
2019-01-19 12:38:31 -0500

    subtract

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,7 @@ public class Calculator {
     public static double add(double n1,double n2){
         return n1+n2;
     }
+    public static double subtract(double n1,double n2){
+        return n1-n2;
+    }
 }
